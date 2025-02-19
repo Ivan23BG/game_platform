3780a71c5246db34cb62baf60ded1ba7511d7037
Ivan23BG <ilejeune@outlook.com>
2025-02-19 15:14:52 +0100

    Initial commit: Basic structure for game platform with Flask

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/game1')
+def game1():
+    return render_template('game1.html')
+
+@app.route('/game2')
+def game2():
+    return render_template('game2.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
